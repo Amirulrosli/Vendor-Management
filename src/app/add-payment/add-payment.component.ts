fb5f0351323b161129f2f5392f62b03f21b98434
Ikhwan Firdaus <18FTT1601@student.pb.edu.bn>
2021-01-21 10:00:32 +0800

    wan

--- src/app/add-payment/add-payment.component.ts
+++ src/app/add-payment/add-payment.component.ts
@@ -1,7 +1,16 @@
-import { CurrencyPipe } from '@angular/common';
+// import { CurrencyPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { profileService } from '../services/profile.service';
+import { paymentService } from '../services/payment.service';
+// import { Profile } from '../services/Profile.model';
+// import { identifierModuleUrl } from '@angular/compiler';
+
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+
+
 
 @Component({
   selector: 'app-add-payment',
@@ -11,15 +20,38 @@ import { profileService } from '../services/profile.service';
 export class AddPaymentComponent implements OnInit {
   close: any;
   opened = true
-  date: any;
+  date: Date;
+  retrieveData:any;
+  retrieveDataLength:any;
+  id;
+  value;
+  selectField;
+
+  info;
+
+  dateField:Date;
+  priceField;
+  slotField;
+  vendorField;
+  
+
+  list: any = []
+
   // formattedAmount;
 
   constructor(
     private router: Router,
     private profile : profileService,
+    private payment: paymentService,
+    private _snackBar: MatSnackBar
+    
     // private currencyPipe : CurrencyPipe
   ) {
     this.close = false;
+
+    this.selectField = null;
+
+    
    }
 
 //    transformAmount(element){
@@ -29,7 +61,22 @@ export class AddPaymentComponent implements OnInit {
 // }
 
   ngOnInit(): void {
+    this.profile.findAll().subscribe(array => {
+      this.retrieveData = array
+      
+      
+      console.log(array)
+      // this.list = array.map(item => {
+      //   console.log(item)
+      // })
+      
+    })
+
+    // var newDate2 = new Date();
+    // console.log(newDate2)
   }
+
+  
 
   goToDashboard(){
     console.log("hantu")
@@ -45,8 +92,57 @@ export class AddPaymentComponent implements OnInit {
   }
 
   submit(){
-    console.log("submit")
+    // this.date = new Date(this.dateField.setMonth(this.dateField.getMonth()+1));
+    // var newDate2 = new Date();
+    // console.log(newDate2)
+
+    //need to parse date, date returns as a string.
+
+    this.info = {
+      payment_Date: this.dateField, 
+      due_Date: new Date(),  //change to calculation of next 30days
+      email: this.list.email,
+      send_Email: false,
+      rid: this.list.rid
+    }
+
+    this.payment.create(this.info).subscribe(array => {
+      console.log(array);
+
+      this.dateField=undefined;
+      this.selectField = "";
+      this.slotField = "";
+      this.priceField = undefined;
+      this.vendorField = "";
+
+      Swal.fire('Hi', 'We have been informed!', 'success')
+      // this._snackBar.open("Data updated", "OK", {
+      //   duration: 10000,
+      // });
+
+    },error => {
+      console.log(error + "salah bui")
+    });
   }
+
+  // select(id){
+  //   this.list = this.retrieveData[id];
+  //   console.log(this.list)
+  // }
+
+//   onChange(deviceValue) {
+//     console.log(this.retrieveData)
+//     this.list = this.retrieveData[deviceValue-1];
+//     console.log(this.list)
+//     console.log(deviceValue);
+// }
+
+onChange(deviceValue) {
+  this.profile.findOne(deviceValue).subscribe(array =>{
+    console.log(array)
+    this.list = array;
+  })
+}
 
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'vendorManagement';
   opened = true;
+
+
+  // successNotification(){
+  //   Swal.fire('Hi', 'We have been informed!', 'success')
+  // }
+
 }
