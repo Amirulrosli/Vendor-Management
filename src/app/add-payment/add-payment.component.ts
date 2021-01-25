f69483c11653f5e2785bd56115df3d2a1bb2d639
Amirulrosli <60535207+Amirulrosli@users.noreply.github.com>
2021-01-25 10:57:56 +0800

    commit 25/1 10:57

--- src/app/add-payment/add-payment.component.ts
+++ src/app/add-payment/add-payment.component.ts
@@ -102,108 +102,243 @@ onChange(deviceValue) {
   })
 }
 
+compareData(dueDate){
+
+  try{
+
+    this.payment.findByRid(this.list.rid).subscribe(data=> {
+      
+      this.userData = data;
+
+      console.log(this.userData)
+
+
+      if (this.userData.length == 0){
+        this.list.latest_Payment_Date = this.dateField;
+        this.list.latest_Payment = this.priceField;
+        this.list.latest_Due_Date = dueDate;
+      } else {
+
+
+        var paymentDate = new Date(this.dateField);
+        var paymentDay = paymentDate.getDate();
+        var paymentMonth = paymentDate.getMonth()+1;
+        var paymentYear = paymentDate.getFullYear();
+    
+
+
+
+        var newDate;
+        var newDay;
+        var newMonth;
+        var newDueDate;
+        var newPrice;
+        var newYear;
+        for(let i = 0; i<this.userData.length;i++){  //loops for latest payment
+        
+          var date = new Date(this.userData[i].payment_Date);
+          var day = date.getDate();
+          var month = date.getMonth()+1;
+          var year = date.getFullYear();
+
+          
+
+          if (newDate == undefined){
+            newDate = this.dateField;
+            newDay = paymentDay;
+            newMonth = paymentMonth;
+            newYear = paymentYear;
+            newDueDate = dueDate;
+            newPrice = this.priceField;
+
+          }
+
+          
+
+          if (day <= newDay){ //if day high
+            if (month <= newMonth){ //month low
+
+              if (year <= newYear){
+
+                newDate = newDate;
+                newDay = newDay;
+                newMonth = newMonth;
+                newYear = newYear;
+                newDueDate = newDueDate;
+                newPrice = newPrice;
+                console.log("set 1")
+                
+
+              } else {
+
+                newDate = date;
+                newDay = day;
+                newMonth = month;
+                newYear = year;
+                newDueDate = this.userData[i].due_Date;
+                newPrice = this.userData[i].price;
+                console.log("set 4")
+
+              }
+
+            } else if (year <= newYear) {
+                
+              newDate = newDate;
+              newDay = newDay;
+              newMonth = newMonth;
+              newYear = newYear;
+              newDueDate = newDueDate;
+              newPrice = newPrice;
+              console.log("set 1")
+
+            } else {
+
+              newDate = date;
+              newDay = day;
+              newMonth = month;
+              newYear = year;
+              newDueDate = this.userData[i].due_Date;
+              newPrice = this.userData[i].price;
+              console.log("set 4")
+
+            }
+          } else if (month <= newMonth) {
+            if (year <= newYear){
+
+              
+              newDate = newDate;
+              newDay = newDay;
+              newMonth = newMonth;
+              newYear = newYear;
+              newDueDate = newDueDate;
+              newPrice = newPrice;
+              console.log("set 1")
+
+            } else {
+              newDate = date;
+              newDay = day;
+              newMonth = month;
+              newYear = year;
+              newDueDate = this.userData[i].due_Date;
+              newPrice = this.userData[i].price;
+              console.log("set 4")
+            }
+          } else if (year <= newYear){
+
+            
+            newDate = newDate;
+            newDay = newDay;
+            newMonth = newMonth;
+            newYear = newYear;
+            newDueDate = newDueDate;
+            newPrice = newPrice;
+            console.log("set 1")
+
+          } else {
+              newDate = date;
+              newDay = day;
+              newMonth = month;
+              newYear = year;
+              newDueDate = this.userData[i].due_Date;
+              newPrice = this.userData[i].price;
+              console.log("set 4")
+          }
+        }
+        this.list.latest_Payment_Date = newDate;
+        this.list.latest_Due_Date = newDueDate;
+        this.list.latest_Payment = newPrice;
+      }
+     
+
+  
+
+
+      this.info = {
+        payment_Date: this.dateField, 
+        due_Date: dueDate,  
+        email: this.list.email,
+        send_Email: false,
+        rid: this.list.rid,
+        price: this.priceField
+      }
+  
+      this.lastPayment ={
+        latest_Payment_Date: this.dateField
+      }
+  
+  
+      //validation
+      if(this.selectField == null){
+        this.formStatus = false;
+        Swal.fire('Error', 'Please select an IC number!', 'error')
+  
+      }
+      else if(this.dateField == null){
+        this.formStatus = false;
+        Swal.fire('Error', 'Please enter a valid date!', 'error')
+  
+      }
+      else if(this.priceField == null){
+        this.formStatus = false;
+        Swal.fire('Error', 'Please enter a price!', 'error')
+  
+      }
+      else if(this.priceField == null && this.dateField == null && this.selectField == null){
+        this.formStatus = false;
+        this.failed()
+      }
+  
+      
+      if(this.formStatus == true){
+  
+         //update latest payment
+         this.profile.update(this.list.id,this.list).subscribe(array =>{
+          console.log("successfully update profile");
+        })
+  
+        this.payment.create(this.info).subscribe(array => {
+          console.log("successfully create payment");
+    
+          this.dateField=undefined;
+          this.selectField = "";
+          this.slotField = "";
+          this.priceField = undefined;
+          this.vendorField = "";
+       
+          this.list = [];
+        
+          this.alert.successNotification();
+
+    
+        },error => {
+          console.log(error + "salah bui")
+        });
+      }
+    })
+
+  } catch (err){
+
+
+    this.list.latest_Payment_Date = this.dateField;
+    this.list.latest_Payment = this.priceField;
+    this.list.latest_Due_Date = dueDate;
+
+  }
+
+}
+
   submit(){
     this.formStatus = true;
   
     //add one month to date
     var dueDate = this.addMonths(new Date (this.dateField),1);
-    let latest_dueDate = this.datePipe.transform(dueDate, 'dd-MM-yyyy');
+    // let latest_dueDate = this.datePipe.transform(dueDate, 'dd-MM-yyyy');
 
+    return new Promise (async (resolve)=> {
 
-    // try{
+      var compare = await this.compareData(dueDate); 
 
-    //   this.payment.findByRid(this.list.rid).subscribe(data=> {
-        
-    //     this.userData = data;
-
-
-    //     for(let i = 0; i<this.userData.length;i++){
-        
-    //       const day = this.list.latest_Payment_Date
-          
-
-    //     }
-    //   })
-
-    // } catch (err){
-
-    // }
-
-
-
-    this.info = {
-      payment_Date: this.dateField, 
-      due_Date: dueDate,  
-      email: this.list.email,
-      send_Email: false,
-      rid: this.list.rid,
-      price: this.priceField
-    }
-
-    this.lastPayment ={
-      latest_Payment_Date: this.dateField
-    }
-
-    this.list.latest_Payment_Date = this.dateField;
-    this.list.latest_Payment = this.priceField;
-
- 
- 
-
-    //validation
-    if(this.selectField == null){
-      this.formStatus = false;
-      Swal.fire('Error', 'Please select an IC number!', 'error')
-
-    }
-
-    else if(this.dateField == null){
-      this.formStatus = false;
-      Swal.fire('Error', 'Please enter a valid date!', 'error')
-
-    }
-
-    else if(this.priceField == null){
-      this.formStatus = false;
-      Swal.fire('Error', 'Please enter a price!', 'error')
-
-    }
-
-    else if(this.priceField == null && this.dateField == null && this.selectField == null){
-      this.formStatus = false;
-      this.failed()
-    }
-    
-    if(this.formStatus == true){
-
-       //update latest payment
-       this.profile.update(this.list.id,this.list).subscribe(array =>{
-        console.log(array)
-      })
-
-      this.payment.create(this.info).subscribe(array => {
-        console.log(array);
-  
-        this.dateField=undefined;
-        this.selectField = "";
-        this.slotField = "";
-        this.priceField = undefined;
-        this.vendorField = "";
-
-        console.log(this.list.id)
-     
-  
-      
-        this.alert.successNotification();
-        
-  
-      },error => {
-        console.log(error + "salah bui")
-      });
-    }
-    
-    
-
+    })
     
   }
 
