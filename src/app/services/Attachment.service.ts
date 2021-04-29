a28db5672a70a7a7b1c248e2be45e5286247bcd5
Amirulrosli <18ftt1642@student.pb.edu.bn>
2021-04-29 16:56:28 +0800

    commit 29/4

---
+++ src/app/services/Attachment.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+
+const baseURL="http://localhost:3000/api/attachment"
+
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class attachmentService {
+
+    constructor( private http : HttpClient){}
+
+    findAll(): Observable<any> {
+        return this.http.get(baseURL)
+    }
+
+    create(data): Observable<any> {
+        console.log(data)
+        return this.http.post(baseURL,data)
+    }
+
+    update(id,data): Observable<any> {
+        return this.http.put(`${baseURL}/update/${id}`,data)
+    }
+
+    deleteAll(): Observable<any>{
+        return this.http.delete(baseURL);
+    }
+
+    delete(id): Observable<any> {
+        return this.http.delete(`${baseURL}/${id}`);
+    }
+
+    findByid(id): Observable<any> {
+        return this.http.get(`${baseURL}/id/${id}`)
+    }
+    findByVendorid(vendor_rid): Observable<any> {
+        return this.http.get(`${baseURL}/vendor_rid/${vendor_rid}`)
+    }
+
+}
--- src/app/usermanagement/usermanagement.component.ts
+++ src/app/usermanagement/usermanagement.component.ts
@@ -1,7 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatSlidePanel } from 'ngx-mat-slide-panel';
 import { NotificationComponent } from '../notification/notification.component';
+import { attachmentService } from '../services/Attachment.service';
 import { notificationService } from '../services/notification.service';
 
 @Component({
@@ -15,11 +17,16 @@ export class UsermanagementComponent implements OnInit {
   opened = true
   notifyData: any = [];
   notifyNo: any;
+  link: any;
+  action = "http://localhost:3000/upload-Profile"
 
   constructor(
     private router: Router,
     private slidePanel: MatSlidePanel,
-    private notification: notificationService
+    private notification: notificationService,
+    private attachment: attachmentService,
+    private http: HttpClient
+
   ) {
     this.close = false;
    }
@@ -55,6 +62,47 @@ export class UsermanagementComponent implements OnInit {
         this.notifyData = data;
         this.notifyNo = this.notifyData.length;
     })
+  }
+
+  change(file){
+
+    this.link = file.target.files[0];
+
+  }
+
+  submit(){
+
+    const vendor_rid = "13y14hbfhgjaf"
+    const account_rid = "121212121"
+    const link = this.link;
+
+    var attachment = {
+      vendor_rid: vendor_rid,
+      account_rid: account_rid,
+      link: link
+    }
+
+    this.http.post(this.action,attachment).subscribe(data=> {
+      console.log(data)
+    })
+
+
+
+
+  //   try{
+
+  //     this.attachment.create(attachment).subscribe(data=> {
+  //       console.log(data)
+  //     },error=> {
+  //       console.log( error)
+  //     })
+
+  //   } catch (error){
+  //     console.log(error)
+  //   }
+
+
+
   }
 
 }
